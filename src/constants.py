757186771abb25f5b5b83d53e864d48af2b32576
J-Negrete <jcnegrete96@gmail.com>
2022-01-13 21:01:50 -0800

    clean up

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
-MY_ADDRESS = "0x35184dea3c72c56c7ee9af992a658cc3f671a901"
-NON_OWNER_ADDRESS = "0x2aF178C12A65FF16AeEADFa39bA4ade053cc769A"
+MY_ADDRESS = ""
+NON_OWNER_ADDRESS = ""
 HACK_ME_CALL_PAUSE_FUNCTION_ABI = '{"inputs":[],"name":"callSensitiveFunction","outputs":[],"stateMutability":"nonpayable","type":"function"}'
 HACK_ME_ABI = [
     {
